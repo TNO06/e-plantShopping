f178df4609091138b1d1484ce11bd832175ed4c9
TNO06 <dhawalkashyap5399@gmail.com>
2025-06-27 17:13:13 -0400

    updatesss

--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
+// Importing Necessary Functions and Files:
+// The configureStore() function from Redux Toolkit is used to create the Redux store.
 import { configureStore } from '@reduxjs/toolkit';
+// The cartReducer from the CartSlice.jsx file manages the cart slice of the Redux state.
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+  // Define the root reducer object
+  reducer: {
+    // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+    cart: cartReducer,
+  },
 });
-export default store
+
+// Exporting the Store:
+// The configured Redux store is exported so it can be used in the app (e.g., in <Provider store={store}>)
+export default store;
